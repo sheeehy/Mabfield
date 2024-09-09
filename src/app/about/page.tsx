005312d830c6e90c7d8e9d30935a1a83450226c3
Jack Sheehy <jackhughsheehy@gmail.com>
2024-09-09 21:31:41 +0100

    Remove Files + About Component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,137 @@
-"use client"
+"use client";
 import React from "react";
 import Navbar from "../components/Navbar";
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ReactLenis } from "lenis/react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Page: React.FC = () => {
+  useEffect(() => {
+    const ScrollTriggerSettings = {
+      trigger: ".main",
+      start: "top 25%",
+      toggleActions: "play reverse play reverse",
+    };
+
+    const leftXValues = [-800, -900, -400];
+    const rightXValues = [800, 900, 400];
+
+    const leftRotationValues = [-30, -20, -35];
+    const rightRotationValues = [30, 20, 35];
+
+    const yValues = [100, -150, -400];
+
+    gsap.utils.toArray<Element>(".row").forEach((row, index) => {
+      // Specified type for toArray
+      const cardLeft = row.querySelector(".card-left") as HTMLElement;
+      const cardRight = row.querySelector(".card-right") as HTMLElement;
+
+      gsap.to(cardLeft, {
+        x: leftXValues[index],
+        scrollTrigger: {
+          trigger: ".main",
+          start: "top center",
+          end: "150% bottom",
+          scrub: true,
+          onUpdate: (self) => {
+            const progress = self.progress;
+            cardLeft.style.transform = `translateX(${
+              progress * leftXValues[index]
+            }px) translateY(${progress * yValues[index]}px) rotate(${
+              progress * leftRotationValues[index]
+            }deg)`;
+
+            cardRight.style.transform = `translateX(${
+              progress * rightXValues[index]
+            }px) translateY(${progress * yValues[index]}px) rotate(${
+              progress * rightRotationValues[index]
+            }deg)`;
+          },
+        },
+      });
+    });
+
+    gsap.to(".logo", {
+      scale: 1,
+      duration: 0.5,
+      ease: "power1.out",
+      scrollTrigger: ScrollTriggerSettings,
+    });
+
+    gsap.to(".line p", {
+      y: 0,
+      stagger: 0.1,
+      duration: 0.5,
+      ease: "power1.out",
+      scrollTrigger: ScrollTriggerSettings,
+    });
+
+    gsap.to(".button", {
+      y: 0,
+      opacity: 1,
+      delay: 0.25,
+      duration: 0.5,
+      ease: "power1.out",
+      scrollTrigger: ScrollTriggerSettings,
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, []);
+
+  const generateRows = () => {
+    const rows = [];
+
+    for (let i = 1; i <= 3; i++) {
+      rows.push(
+        <div className="row" key={i}>
+          <div className="card card-left">
+            <img src={`/img-${2 * i - 1}.jpg`} alt="" />
+          </div>
+          <div className="card card-right">
+            <img src={`/img-${2 * i}.jpg`} alt="" />
+          </div>
+        </div>
+      );
+    }
+    return rows;
+  };
+
   return (
     <div>
-      <Navbar animationDelay={0}/>
-    <div className="flex justify-center items-center my-64">
-      <h1 className="font-[900] text-7xl ">ABOUT</h1>{" "}
-    </div>
+      <Navbar animationDelay={0} />
+      <ReactLenis root>
+        <section className="hero"></section>
+        <section className="main">
+          <div className="main-content">
+            <div className="logo">
+              <img src="logo.png" alt="" />
+            </div>
+
+            <div className="copy ">
+              <div className="line">
+                <p>Alternative sounds via Ireland.</p>
+              </div>
+              <div className="line">
+                <p>Alternative sounds via Ireland.</p>
+              </div>
+              <div className="line">
+                <p>Alternative sounds via Ireland.</p>
+              </div>
+            </div>
+
+            <div className="btn">
+              <button>Latest Episodes</button>
+            </div>
+          </div>
+          {generateRows()}
+        </section>
+        <section className="footer"></section>
+      </ReactLenis>
     </div>
   );
 };
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
-import { FaXTwitter, FaYoutube, FaInstagram, FaSpotify, FaTiktok, FaDiscord } from "react-icons/fa6";
+import {
+  FaXTwitter,
+  FaYoutube,
+  FaInstagram,
+  FaSpotify,
+  FaTiktok,
+  FaDiscord,
+} from "react-icons/fa6";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { useSpring, animated } from "react-spring";
 import Image from "next/image";
@@ -54,12 +61,12 @@ const Navbar: React.FC<NavbarProps> = ({ animationDelay = 0 }) => {
         tl.fromTo(
           links,
           { opacity: 0, y: 0 },
-          { 
-            opacity: 1, 
-            y: 0, 
-            duration: 0.8, 
-            stagger: 0.05, 
-            ease: "back.out(1.7)"
+          {
+            opacity: 1,
+            y: 0,
+            duration: 0.8,
+            stagger: 0.05,
+            ease: "back.out(1.7)",
           },
           "-=0.4"
         );
@@ -77,13 +84,27 @@ const Navbar: React.FC<NavbarProps> = ({ animationDelay = 0 }) => {
 
       if (isMenuOpen) {
         gsap.to(menu, { x: "0%", duration: 0.5, ease: "power3.out" });
-        gsap.fromTo(menuItems, 
+        gsap.fromTo(
+          menuItems,
           { y: 20, opacity: 0 },
-          { y: 0, opacity: 1, duration: 0.5, stagger: 0.1, ease: "power3.out", delay: 0.2 }
+          {
+            y: 0,
+            opacity: 1,
+            duration: 0.5,
+            stagger: 0.1,
+            ease: "power3.out",
+            delay: 0.2,
+          }
         );
       } else {
         gsap.to(menu, { x: "100%", duration: 0.5, ease: "power3.in" });
-        gsap.to(menuItems, { y: 20, opacity: 0, duration: 0.3, stagger: 0.05, ease: "power3.in" });
+        gsap.to(menuItems, {
+          y: 20,
+          opacity: 0,
+          duration: 0.3,
+          stagger: 0.05,
+          ease: "power3.in",
+        });
       }
     }
   }, [isMenuOpen]);
@@ -97,35 +118,73 @@ const Navbar: React.FC<NavbarProps> = ({ animationDelay = 0 }) => {
       <div className="text-[#797979] text-sm font-[500]">
         <div className="flex justify-between items-center px-4 lg:px-8 pt-6 bg-transparent">
           {/* Left: Social Media Icons (hidden on mobile) */}
-          <div ref={socialsRef} className="hidden lg:flex flex-shrink-0 justify-center items-center gap-5 text-lg">
+          <div
+            ref={socialsRef}
+            className="hidden lg:flex flex-shrink-0 justify-center items-center gap-5 text-lg"
+          >
             <div className="hover:opacity-70 transition ease-in-out">
-            <a href="https://www.youtube.com/c/Mabfield" target="_blank" className="hover:opacity-70 transition ease-in-out social-link "><FaYoutube /></a>
+              <a
+                href="https://www.youtube.com/c/Mabfield"
+                target="_blank"
+                className="hover:opacity-70 transition ease-in-out social-link "
+              >
+                <FaYoutube />
+              </a>
             </div>
             <div className="hover:opacity-70 transition ease-in-out">
-            <a href="https://www.instagram.com/mabfield/" target="_blank" className="social-link "><FaInstagram /></a>
+              <a
+                href="https://www.instagram.com/mabfield/"
+                target="_blank"
+                className="social-link "
+              >
+                <FaInstagram />
+              </a>
             </div>
             <div className="hover:opacity-70 transition ease-in-out">
-            <a href="https://x.com/mabfield_" target="_blank" className="social-link "><FaXTwitter /></a>
+              <a
+                href="https://x.com/mabfield_"
+                target="_blank"
+                className="social-link "
+              >
+                <FaXTwitter />
+              </a>
             </div>
             <div className="hover:opacity-70 transition ease-in-out">
-            <a href="https://open.spotify.com/user/mabfield" target="_blank" className="social-link "><FaSpotify /></a>
+              <a
+                href="https://open.spotify.com/user/mabfield"
+                target="_blank"
+                className="social-link "
+              >
+                <FaSpotify />
+              </a>
             </div>
             <div className="hover:opacity-70 transition ease-in-out">
-            <a href="https://www.tiktok.com/@mabfield" target="_blank" className="social-link "><FaTiktok /></a>
+              <a
+                href="https://www.tiktok.com/@mabfield"
+                target="_blank"
+                className="social-link "
+              >
+                <FaTiktok />
+              </a>
             </div>
             <div className="hover:opacity-70 transition ease-in-out">
-            <a href="/" target="_blank" className="social-link "><FaDiscord /></a>
-</div>
-
+              <a href="/" target="_blank" className="social-link ">
+                <FaDiscord />
+              </a>
+            </div>
           </div>
 
           {/* Center: Logo */}
-          <div className="lg:absolute z-50 lg:left-1/2 lg:transform lg:-translate-x-1/2 hover:opacity-70 transition ease-in-out hover:scale-105 ">
-            <a ref={logoRef} href="/" className={`${mavenPro.className} font-[900] text-black text-xl`}>
+          <div className="lg:absolute z-50 lg:left-1/2 lg:transform lg:-translate-x-1/2 hover:opacity-70 transition ease-in-out hover:scale-[0.8] scale-75">
+            <a
+              ref={logoRef}
+              href="/"
+              className={`${mavenPro.className} font-[900] text-black text-xl`}
+            >
               <Image
                 src="/mabfieldWordmark.png"
                 alt="Mabfield Logo"
-                className="opacity-0"
+                className="opacity-0   "
                 width={150}
                 height={150}
                 quality={100}
@@ -136,29 +195,45 @@ const Navbar: React.FC<NavbarProps> = ({ animationDelay = 0 }) => {
           </div>
 
           {/* Right: Navigation Links (hidden on mobile) */}
-          <div ref={linksRef} className="hidden lg:block flex-shrink-0 text-[#797979] font-[700]">
+          <div
+            ref={linksRef}
+            className="hidden lg:block flex-shrink-0 text-[#797979] font-[700]"
+          >
             <div className="flex justify-center items-center gap-12">
               <div className="hover:opacity-75 transition ease-in-out ">
-              <a href="/listen" className=" ">LISTEN</a>
+                <a href="/listen" className=" ">
+                  LISTEN
+                </a>
               </div>
               <div className="hover:opacity-75 transition ease-in-out ">
-
-              <a href="/episodes" className=" ">EPISODES</a>
+                <a href="/episodes" className=" ">
+                  EPISODES
+                </a>
               </div>
               <div className="hover:opacity-75 transition ease-in-out ">
-
-              <a href="/about" className=" ">ABOUT</a>
+                <a href="/about" className=" ">
+                  ABOUT
+                </a>
               </div>
             </div>
           </div>
 
           {/* Burger Menu (visible only on mobile) */}
           <div className="lg:hidden">
-            <button onClick={toggleMenu} className="text-[#797979] text-2xl z-50 relative w-8 h-8">
-              <animated.div style={menuIconAnimation} className="absolute inset-0 flex items-center justify-center">
+            <button
+              onClick={toggleMenu}
+              className="text-[#797979] text-2xl z-50 relative w-8 h-8"
+            >
+              <animated.div
+                style={menuIconAnimation}
+                className="absolute inset-0 flex items-center justify-center"
+              >
                 <FaBars />
               </animated.div>
-              <animated.div style={closeIconAnimation} className="absolute inset-0 flex items-center justify-center">
+              <animated.div
+                style={closeIconAnimation}
+                className="absolute inset-0 flex items-center justify-center"
+              >
                 <FaTimes />
               </animated.div>
             </button>
@@ -167,22 +242,62 @@ const Navbar: React.FC<NavbarProps> = ({ animationDelay = 0 }) => {
       </div>
 
       {/* Full Screen Mobile Menu */}
-      <div 
-        ref={menuRef} 
+      <div
+        ref={menuRef}
         className="lg:hidden fixed inset-0 bg-white z-40 flex flex-col justify-center transform translate-x-full"
       >
-        <div ref={menuItemsRef} className="flex flex-col items-start gap-8 text-black font-[700] mb-12 px-8">
-          <a href="/listen" className="text-3xl" onClick={toggleMenu}>LISTEN</a>
-          <a href="/episodes" className="text-3xl" onClick={toggleMenu}>EPISODES</a>
-          <a href="/about" className="text-3xl" onClick={toggleMenu}>ABOUT</a>
+        <div
+          ref={menuItemsRef}
+          className="flex flex-col items-start gap-8 text-black font-[700] mb-12 px-8"
+        >
+          <a href="/listen" className="text-3xl" onClick={toggleMenu}>
+            LISTEN
+          </a>
+          <a href="/episodes" className="text-3xl" onClick={toggleMenu}>
+            EPISODES
+          </a>
+          <a href="/about" className="text-3xl" onClick={toggleMenu}>
+            ABOUT
+          </a>
           <div className="flex justify-start items-center gap-8 text-2xl mt-8 text-[#797979]">
-            <a href="https://www.youtube.com/c/Mabfield" target="_blank" className="social-link"><FaYoutube /></a>
-            <a href="https://www.instagram.com/mabfield/" target="_blank" className="social-link"><FaInstagram /></a>
-            <a href="https://x.com/mabfield_" target="_blank" className="social-link"><FaXTwitter /></a>
-            <a href="https://open.spotify.com/user/mabfield" target="_blank" className="social-link"><FaSpotify /></a>
-            <a href="https://www.tiktok.com/@mabfield" target="_blank" className="social-link"><FaTiktok /></a>
-            <a href="/" target="_blank" className="social-link"><FaDiscord /></a>
-
+            <a
+              href="https://www.youtube.com/c/Mabfield"
+              target="_blank"
+              className="social-link"
+            >
+              <FaYoutube />
+            </a>
+            <a
+              href="https://www.instagram.com/mabfield/"
+              target="_blank"
+              className="social-link"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="https://x.com/mabfield_"
+              target="_blank"
+              className="social-link"
+            >
+              <FaXTwitter />
+            </a>
+            <a
+              href="https://open.spotify.com/user/mabfield"
+              target="_blank"
+              className="social-link"
+            >
+              <FaSpotify />
+            </a>
+            <a
+              href="https://www.tiktok.com/@mabfield"
+              target="_blank"
+              className="social-link"
+            >
+              <FaTiktok />
+            </a>
+            <a href="/" target="_blank" className="social-link">
+              <FaDiscord />
+            </a>
           </div>
         </div>
       </div>
